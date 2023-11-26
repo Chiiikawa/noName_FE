a53bf3b802e4b063f1f8a010dbf7932b2c683785
Chiiikawa <chuu.doe@proton.me>
2023-11-26 15:27:47 +0900

    Init: UI/UX

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import Router from "./Router.jsx";
+import Main from "./Pages/Main.jsx";
+
+function App() {
+  return (
+    <div>
+      <Router />
+      <Main />
+    </div>
+  );
+}
+
+export default App;

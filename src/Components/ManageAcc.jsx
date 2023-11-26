de9b4fe272bd6172b18a561e53d4de93352f4ec0
Chiiikawa <chuu.doe@proton.me>
2023-11-26 15:26:17 +0900

    Init: Design Components

---
+++ src/Components/ManageAcc.jsx
@@ -0,0 +1,8 @@
+// The usage of this component is to pop a managing-account modal. Also, you should check the authentication that users logged in.
+import React from "react";
+
+function ManageAcc{
+
+}
+
+export default ManageAcc;

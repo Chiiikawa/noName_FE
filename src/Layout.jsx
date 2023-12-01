f75147f5ce34bbe67d6a859f3843a12b75449666
GSC <141592320+tkdcks7@users.noreply.github.com>
2023-12-01 16:49:29 +0900

    Fix: Mypage rendering problem

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -8,6 +8,9 @@ import Result from "./Pages/Result.jsx";
 import Mainguest from "./Pages/Mainguest.jsx";
 import ContactUs from "./Pages/ContactUs.jsx";
 import Error from "./Pages/Error.jsx";
+import SignUp from "./Pages/Signup.jsx";
+import Mypage from "./Pages/Mypage.jsx"
+
 
 const Layout = () => {
   return (
@@ -20,7 +23,9 @@ const Layout = () => {
       <Routes>
       <Route index element={<Main />}/>
       <Route path="mainguest" element={<Mainguest />} />
+      <Route path="signup" element={<SignUp />} />
       <Route path="result" element={<Result />} />
+      <Route path="mypage" element={<Mypage />} />
       <Route path="contact" element={<ContactUs />} />
       <Route path="/*" element={<Navigate to={"/error"} />} />
       </Routes>

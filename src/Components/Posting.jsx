13f8b499d72e0d9934b4015a46fc934b7e9d2033
GSC <141592320+tkdcks7@users.noreply.github.com>
2023-12-01 16:43:18 +0900

    Fix: Posting component image problem

--- src/Components/Posting.jsx
+++ src/Components/Posting.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css'
 
 // Post에 image가 존재할 시 image를 띄우고, 아니면 기본 이미지 출력
 function Posting({ id, image, title }) {
+
   return (
     <div className='post'>
-        {{image}
-        ?<img src={`${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}${image}`} alt='No Image'/>
-        :<img src='../../public/apology.jpeg' />}
-        <div>{title}</div>
+      <img
+        src={`${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}${image}`}
+        alt='No Image'
+      />
+      <div>{title}</div>
     </div>
   )
 }
